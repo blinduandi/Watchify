55757c6e43026ba2ac7e7339ec6dc78f6ca63837
caramisca <caramisca2@gmail.com>
2024-09-15 23:37:58 +0300

    second commit

--- Watchify/Watchify/Views/MainPage.xaml.cs
+++ Watchify/Watchify/Views/MainPage.xaml.cs
@@ -2,11 +2,20 @@ namespace Watchify.Views;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-       
+    public MainPage()
+    {
         InitializeComponent();
-	}
+        AnimateCircle();
+    }
 
-  
+    private async void AnimateCircle()
+    {
+        while (true)
+        {
+            await GradientCircle.ScaleTo(1.2, 1000, Easing.Linear);
+            await GradientCircle.ScaleTo(1.0, 1000, Easing.Linear);
+        }
+    }
+
+
 }
--- Watchify/Watchify/obj/Debug/net8.0-android/Watchify.AssemblyInfo.cs
+++ Watchify/Watchify/obj/Debug/net8.0-android/Watchify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Watchify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1098f1d8478cde6817ef7f9eb929baf77d6fb37f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3c2f9b02367594f7f83562b38aeb1d55e1f27453")]
 [assembly: System.Reflection.AssemblyProductAttribute("Watchify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Watchify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
