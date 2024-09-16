5cdf3f05cdf356f1fa98b067f28e0d50a98130dd
blinduandi <67801991+blinduandi@users.noreply.github.com>
2024-09-16 11:42:33 +0300

    Com

--- Watchify/Watchify/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Watchify.AssemblyInfo.cs
+++ Watchify/Watchify/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Watchify.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Watchify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0c9bfba8ad3b54a83da147c960898a09de7f1f50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1098f1d8478cde6817ef7f9eb929baf77d6fb37f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Watchify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Watchify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
